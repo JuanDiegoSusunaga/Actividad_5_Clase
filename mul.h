f3acb7f36b52bb95f36daf139809b1c4c03e37e3
JuanDiegoSusunaga <138306321+JuanDiegoSusunaga@users.noreply.github.com>
2025-05-12 21:15:29 -0600

    Update mul.h

--- mul.h
+++ mul.h
@@ -3,4 +3,13 @@
 
 int multiply(int a, int b);
 
+/**
+ * @brief Multiplica dos números enteros y retorna el resultado como un entero.
+ * 
+ * @param a Primer número (factor).
+ * @param b Segundo número (factor).
+ * @return int Resultado de la multiplicación de a y b.
+ */
+
+
 #endif  // MUL_H
