d88cfe5e429e690a08c7d48293c785efab7268e7
JuanDiegoSusunaga <138306321+JuanDiegoSusunaga@users.noreply.github.com>
2025-05-12 23:00:01 -0600

    Update test_mult.cpp

--- test/test_mult.cpp
+++ test/test_mult.cpp
@@ -6,10 +6,3 @@ TEST(MulTest, HandlesPositiveInput)
   int multiplicacion = multiply(1, 5);
   EXPECT_EQ(multiplicacion, 6);
 }
-
-TEST(MulTest, HandlesNegativeInput)
-{
-  int multiplicacion = multiply(-2,-4);
-  EXPECT_EQ(multiplicacion, 8);
-
-}
