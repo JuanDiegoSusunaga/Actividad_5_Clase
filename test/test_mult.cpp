2a4d614f87bab863d6e015a5e4724e3c7b13c607
JuanDiegoSusunaga <138306321+JuanDiegoSusunaga@users.noreply.github.com>
2025-05-12 21:33:49 -0600

    Update test_mult.cpp

--- test/test_mult.cpp
+++ test/test_mult.cpp
@@ -1,34 +1,9 @@
 #include <gtest/gtest.h>
 #include "mul.h"
 
-// Multiplicación de dos números positivos
-TEST(MulTest, HandlesPositiveInput) {
-    int result = mul(9, 3);
-    EXPECT_EQ(result, 27);
-}
+TEST(MulTest, HandlesPositiveInput)
+{
+int multriplicacion = sum(1, 5);
 
-// Multiplicación por cero (segundo operando)
-TEST(MulTest, HandlesZeroInput) {
-    int result = mul(9, 0);
-    EXPECT_EQ(result, 0);
+EXPECT_EQ(multriplicacion, 6);
 }
-
-// Multiplicación con número negativo
-TEST(MulTest, HandlesNegativeInput) {
-    int result = mul(-2, 4);
-    EXPECT_EQ(result, -8);
-}
-
-// Multiplicación de dos negativos
-TEST(MulTest, HandlesDoubleNegativeInput) {
-    int result = mul(-3, -5);
-    EXPECT_EQ(result, 15);
-}
-
-// Multiplicación con primer operando cero
-TEST(MulTest, HandlesZeroFirstInput) {
-    int result = mul(0, 7);
-    EXPECT_EQ(result, 0);
-}
-
-// Created by x on 12/05/2025.
