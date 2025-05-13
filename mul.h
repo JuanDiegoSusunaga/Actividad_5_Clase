09e1607dcd8f61505a44a74dd9072bf3b5b3c7c7
JuanDiegoSusunaga <138306321+JuanDiegoSusunaga@users.noreply.github.com>
2025-05-12 20:59:33 -0600

    Update mul.h

--- mul.h
+++ mul.h
@@ -6,3 +6,18 @@ inline int multiply(int a, int b) {
 }
 
 #endif
+
+#include <gtest/gtest.h>
+#include "mul.h"
+
+TEST(MulTest, HandlesPositiveInput) {
+    EXPECT_EQ(multiply(4, 5), 20);
+}
+
+TEST(MulTest, HandlesNegativeInput) {
+    EXPECT_EQ(multiply(-4, 5), -20);
+}
+
+TEST(MulTest, HandlesZeroInput) {
+    EXPECT_EQ(multiply(0, 5), 0);
+}
