d2f00d77dadefb03c091fee10b6ca0e4b66df08b
JuanDiegoSusunaga <138306321+JuanDiegoSusunaga@users.noreply.github.com>
2025-05-12 21:13:04 -0600

    Update div.h

--- div.h
+++ div.h
@@ -1,22 +1,13 @@
-
-/*
- * Copyright (C) 2025 Tec de Monterrey
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
-#include <math.h>
-#include <stdexcept>
+#ifndef DIV_H
+#define DIV_H
 
 /**
-  metodo para realizar divisiones de dos numeros
+ * @brief Divide dos números enteros y retorna el resultado como double.
+ * 
+ * @param x Numerador
+ * @param y Denominador
+ * @return double Resultado de la división. Si y == 0, retorna 0.0.
  */
-double divide(int x, int y)
-{
-    if (y != 0)
-        return x / y;
-    else
-        return 0;
-}
+double divide(int x, int y);
+
+#endif  // DIV_H
