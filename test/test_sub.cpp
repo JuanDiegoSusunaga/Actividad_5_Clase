ea612e31d37a1473ccb19f33ed13661d75143d03
JuanDiegoSusunaga <138306321+JuanDiegoSusunaga@users.noreply.github.com>
2025-05-12 21:32:49 -0600

    Update test_sub.cpp

--- test/test_sub.cpp
+++ test/test_sub.cpp
@@ -1,26 +1,9 @@
 #include <gtest/gtest.h>
 #include "sub.h"
 
-// Test con números positivos
-TEST(SubTest, HandlesPositiveInput) {
-    int resta = sub(5, 1);
-    EXPECT_EQ(resta, 4);
-}
+TEST(SubTest, HandlesPositiveInput)
+{
+int resta = sub(5, 1);
 
-// Test con resultado negativo
-TEST(SubTest, HandlesNegativeResult) {
-    int resta = sub(3, 8);
-    EXPECT_EQ(resta, -5);
-}
-
-// Test con ceros
-TEST(SubTest, HandlesZeroInput) {
-    int resta = sub(0, 0);
-    EXPECT_EQ(resta, 0);
-}
-
-// Test con ambos negativos
-TEST(SubTest, HandlesNegativeInput) {
-    int resta = sub(-3, -2);
-    EXPECT_EQ(resta, -1);
+EXPECT_EQ(resta, 4);
 }
